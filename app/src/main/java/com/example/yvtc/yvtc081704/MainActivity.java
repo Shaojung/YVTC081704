5294a9768781f5039ebe4d029b73b446ea1c8bba
Shaojung <shaojung@gmail.com>
2017-08-22 08:39:24 +0800

    DAO

--- app/src/main/java/com/example/yvtc/yvtc081704/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc081704/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.yvtc.yvtc081704;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.yvtc.yvtc081704.data.*;
 
 import com.example.yvtc.yvtc081704.data.StudentDAOMemoryImpl;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
+    String data[];
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
         StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
         Student stu1 = new Student(1, "Bob", "123123");
         Student stu2 = new Student(2, "Jack", "345345");
@@ -25,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.d("DAO", s.toString());
         }
+        data = new String[students.length];
+        int i;
+        for (i=0;i<students.length;i++)
+        {
+            data[i] = students[i].name;
+        }
+        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, data);
+        lv.setAdapter(adapter);
     }
 }
