42e5f107b5417296067acbd0edd73f724c905cdb
Shaojung <shaojung@gmail.com>
2017-08-22 11:01:57 +0800

    DAO with Update

--- app/src/main/java/com/example/yvtc/yvtc081704/DetailActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc081704/DetailActivity.java
@@ -4,33 +4,35 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.yvtc.yvtc081704.data.Student;
 import com.example.yvtc.yvtc081704.data.StudentDAOMemoryImpl;
 
 public class DetailActivity extends AppCompatActivity {
-    TextView tv1, tv2, tv3;
+    TextView tv1;
+    EditText edName, edTel;
     int ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         tv1 = (TextView) findViewById(R.id.textView);
-        tv2 = (TextView) findViewById(R.id.textView2);
-        tv3 = (TextView) findViewById(R.id.textView3);
+        edName = (EditText) findViewById(R.id.edName);
+        edTel = (EditText) findViewById(R.id.edTel);
         Intent it = getIntent();
         ID = it.getIntExtra("ID", 0);
         StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
         Student s = dao.getOneStudent(ID);
         tv1.setText(String.valueOf(s.ID));
-        tv2.setText(s.name);
-        tv3.setText(s.tel);
+        edName.setText(s.name);
+        edTel.setText(s.tel);
     }
-    public void clickEdit(View v)
+    public void clickUpdate(View v)
     {
-        Intent it = new Intent(DetailActivity.this, EditActivity.class);
-        it.putExtra("ID", ID);
-        startActivity(it);
+        StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
+        dao.update(new Student(ID, edName.getText().toString(), edTel.getText().toString()));
+        finish();
     }
 }
