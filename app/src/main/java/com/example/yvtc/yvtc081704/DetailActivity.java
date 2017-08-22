be8a746999edec80d7f8aade0e71cf9091f6fa34
Shaojung <shaojung@gmail.com>
2017-08-22 10:05:12 +0800

    DAO

--- app/src/main/java/com/example/yvtc/yvtc081704/DetailActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc081704/DetailActivity.java
@@ -3,20 +3,26 @@ package com.example.yvtc.yvtc081704;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.yvtc.yvtc081704.data.Student;
 import com.example.yvtc.yvtc081704.data.StudentDAOMemoryImpl;
 
 public class DetailActivity extends AppCompatActivity {
-
+    TextView tv1, tv2, tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        tv1 = (TextView) findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tv3 = (TextView) findViewById(R.id.textView3);
         Intent it = getIntent();
         int ID = it.getIntExtra("ID", 0);
         StudentDAOMemoryImpl dao = new StudentDAOMemoryImpl();
         Student s = dao.getOneStudent(ID);
-
+        tv1.setText(String.valueOf(s.ID));
+        tv2.setText(s.name);
+        tv3.setText(s.tel);
     }
 }
