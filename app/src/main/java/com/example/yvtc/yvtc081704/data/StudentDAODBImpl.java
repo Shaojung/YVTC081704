df976caf1e65158eda8324297890a7c9b8b13c2e
Shaojung <shaojung@gmail.com>
2017-08-23 13:33:08 +0800

    DAO DB implements

--- app/src/main/java/com/example/yvtc/yvtc081704/data/StudentDAODBImpl.java
+++ app/src/main/java/com/example/yvtc/yvtc081704/data/StudentDAODBImpl.java
@@ -1,6 +1,12 @@
 package com.example.yvtc.yvtc081704.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by yvtc on 2017/8/23.
@@ -16,12 +22,28 @@ public class StudentDAODBImpl implements StudentDAO {
 
     @Override
     public void add(Student s) {
-
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put("name", s.name);
+        cv.put("tel", s.tel);
+        db.insert("phone", null, cv);
+        db.close();
     }
 
     @Override
     public Student[] getAllStudents() {
-        return new Student[0];
+        ArrayList<Student> mylist = new ArrayList<>();
+        SQLiteDatabase db = helper.getWritableDatabase();
+        // Cursor c = db.rawQuery("Select * from phone", null);
+        Cursor c = db.query("phone", new String[] {"id", "name", "tel"}, null, null , null, null, "id desc");
+
+        while (c.moveToNext())
+        {
+            mylist.add(new Student(c.getInt(0), c.getString(1), c.getString(2)));
+        }
+        c.close();
+        db.close();
+        return mylist.toArray(new Student[0]);
     }
 
     @Override
